6c6e7cd3adfcc0dd1e62034486c9b3e5029bc3ba
bccalegari <brunocostacallegari@gmail.com>
2024-07-14 18:25:36 -0300

    feat: welcome card day period and actual date as signals

--- src/app/view/pages/home/components/welcome-card/welcome-card.component.ts
+++ src/app/view/pages/home/components/welcome-card/welcome-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation, WritableSignal, signal } from '@angular/core';
 import { CardModule } from 'primeng/card';
 
 @Component({
@@ -11,12 +11,24 @@ import { CardModule } from 'primeng/card';
 })
 export class WelcomeCardComponent implements OnInit {
   @Input() userName!: string;
-  public dayPeriodString!: string;
-  public actualDateString!: string;
+  public dayPeriodString: WritableSignal<string> = signal<string>('');
+  public actualDateString: WritableSignal<string> = signal<string>('');
+  private readonly _dayPeriodRefreshInterval: number = 5 * 3600000 + (3600000 / 2); // 5 hours and 30 minutes
+  private readonly _actualDateStringRefreshInterval: number = 30000; // 30 seconds
 
   public ngOnInit(): void {
-    this.actualDateString = this._getActualDateStringFormatted();
-    this.dayPeriodString = this._getDayPeriodString();
+    this._updateDayPeriod();
+    this._updateActualDate();
+    setInterval(() => this._updateDayPeriod(), this._dayPeriodRefreshInterval);
+    setInterval(() => this._updateActualDate(), this._actualDateStringRefreshInterval);
+  }
+
+  private _updateDayPeriod(): void {
+    this.dayPeriodString.set(this._getDayPeriodString());
+  }
+
+  private _updateActualDate(): void {
+    this.actualDateString.set(this._getActualDateStringFormatted());
   }
 
   private _getDayPeriodString(): string {
@@ -35,9 +47,20 @@ export class WelcomeCardComponent implements OnInit {
 
   private _getActualDateStringFormatted(): string {
     const actualDate: Date = new Date();
+    const actualDateFormatted: Map<string, string> = this._getActualDateFormattedMap(actualDate);
     return `
-      ${actualDate.toLocaleString('default', { day: '2-digit' })} de ${actualDate.toLocaleString('default', { month: 'long' })}
-      de ${actualDate.getFullYear()} - ${actualDate.getHours()}:${actualDate.getMinutes()}
+      ${actualDateFormatted.get('day')} de ${actualDateFormatted.get('month')} de ${actualDateFormatted.get('year')}
+      - ${actualDateFormatted.get('hour')}:${actualDateFormatted.get('minutes')}
     `;
+  }
+
+  private _getActualDateFormattedMap(actualDate: Date): Map<string, string> {
+    return new Map<string, string>([
+      ['day', actualDate.toLocaleString('default', { day: '2-digit' })],
+      ['month', actualDate.toLocaleString('default', { month: 'long' })],
+      ['year', actualDate.toLocaleString('default', { year: 'numeric' })],
+      ['hour', actualDate.toLocaleString('default', { hour: '2-digit' })],
+      ['minutes', actualDate.toLocaleString('default', { minute: '2-digit' })],
+    ]);
   }
 }
